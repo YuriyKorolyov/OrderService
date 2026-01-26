c8739bc4f457440ada485939a416137b556c3e71
Yuri Korolev <iur.corolyov2015@yandex.ru>
2026-01-26 08:36:27 +0500

    Добавьте файлы проекта.

---
+++ OrderService.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderService.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
