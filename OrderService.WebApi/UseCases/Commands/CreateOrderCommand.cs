6ef285c45f2e50151d955d547c30a65a21d117d1
Yuri Korolev <iur.corolyov2015@yandex.ru>
2026-01-28 12:10:05 +0500

    feat: Добавил команду и обработчик для создания заказа

---
+++ OrderService.WebApi/UseCases/Commands/CreateOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using OrderService.WebApi.DTO.Requests;
+using OrderService.WebApi.DTO.Responses;
+
+namespace OrderService.WebApi.UseCases.Commands;
+
+public class CreateOrderCommand : IRequest<OrderResponse>
+{
+    public CreateOrderRequest Request { get; }
+
+    public CreateOrderCommand(CreateOrderRequest request)
+    {
+        Request = request;
+    }
+}
