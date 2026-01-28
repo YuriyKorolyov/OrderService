20f1021e6fdcc452403d6c08005ac76ff5f08bab
Yuri Korolev <iur.corolyov2015@yandex.ru>
2026-01-28 12:16:55 +0500

    feat: Валидация запросов с FluentValidation

---
+++ OrderService.WebApi/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using OrderService.WebApi.DTO.Requests;
+
+namespace OrderService.WebApi.Validators;
+
+public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(x => x.ProductId).GreaterThan(0);
+        RuleFor(x => x.Amount).GreaterThan(0).LessThanOrEqualTo(1000);
+        RuleFor(x => x.EmailClient).NotEmpty().EmailAddress();
+        RuleFor(x => x.Price).GreaterThan(0);
+        RuleFor(x => x.PhoneNumber).NotEmpty().Matches(@"^\+?[1-9]\d{1,14}$");
+    }
+}
